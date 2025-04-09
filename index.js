3e55ef240fb3f489814f1e975a96812d790bee26
unknown <89658666564@mail.ru>
2025-04-09 20:32:11 +0300

    Реализовали функцию

--- index.js
+++ index.js
@@ -4,4 +4,15 @@ class CreateElement {
 		this.className = className
 		this.content = content
 	}
+
+	createNewElement() {
+		const newElement = document.createElement(this.tag)
+		newElement.classList.add(this.className)
+		newElement.textContent = this.content
+
+		document.body.append(newElement)
+	}
 }
+const el = new CreateElement('p', 'paragraph', 'Новый текс!')
+
+el.createNewElement()
